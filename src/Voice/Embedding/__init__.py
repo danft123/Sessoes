b40ff05d65f4549f24b7ac806903337a3e4795d1
Daniel Ide Futata <danielrag@gmail.com>
2025-07-05 14:09:04 -0300

    refactor: Replace LabelFolder call with LabelAndEmbed in main.py and define LabelAndEmbed function for embedding process

--- src/Voice/Embedding/__init__.py
+++ src/Voice/Embedding/__init__.py
@@ -1,4 +1,11 @@
 from .label_audio import main as LabelFolder
 from .embed import main as EmbedLabeledVoiceSegments
 
-__all__ = ["LabelFolder","EmbedLabeledVoiceSegments"]
+def LabelAndEmbed():
+    """
+    Main function to run the embedding process.
+    """
+    LabelFolder()
+    EmbedLabeledVoiceSegments()
+
+__all__ = ["LabelFolder","EmbedLabeledVoiceSegments","LabelAndEmbed"]
