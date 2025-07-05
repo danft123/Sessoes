0f257058e426defd03186a0881bea9a7986f2944
Daniel Ide Futata <danielrag@gmail.com>
2025-07-05 19:00:30 -0300

    refactor: Update audio loading method to use torchaudio and improve tensor handling in voice masking

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.Voice.MaskAndTranscript import MaskAndTranscriptMain
 from src.Voice.Embedding import LabelFolder, LabelAndEmbed, EmbedLabeledVoiceSegments
 
-# MaskAndTranscriptMain()  # Call the main function to start the process
+MaskAndTranscriptMain()  # Call the main function to start the process
 # LabelAndEmbed()
 # LabelFolder()  # Call the labeling function to label audio segments
-EmbedLabeledVoiceSegments()
+# EmbedLabeledVoiceSegments()
