9424d58c0583041329b2fca5dc8443d2af9b1e43
Daniel Ide Futata <danielrag@gmail.com>
2025-08-09 18:49:56 -0300

    refactor: Update .gitignore and requirements, and implement audio processing in main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from src.Voice.MaskAndTranscript import MaskAndTranscriptMain
-from src.Voice.Embedding import LabelFolder, LabelAndEmbed, EmbedLabeledVoiceSegments
+# from src.Voice.MaskAndTranscript import MaskAndTranscriptMain
+# from src.Voice.Embedding import LabelFolder, LabelAndEmbed, EmbedLabeledVoiceSegments
 
-MaskAndTranscriptMain()  # Call the main function to start the process
+# #MaskAndTranscriptMain()  # Call the main function to start the process
 # LabelAndEmbed()
 # LabelFolder()  # Call the labeling function to label audio segments
 # EmbedLabeledVoiceSegments()
+
+from src.Voice.Transcript.whisperx import transcribe_audio, process_audio_folder
+
+pathfolder = "data/Raw/ProcessedAudios/TEST"
+output_dir = "data/Raw/ProcessedAudios/TEST/Output"
+
+process_audio_folder(pathfolder, output_dir=output_dir)  # Process all audio files in the specified folder
